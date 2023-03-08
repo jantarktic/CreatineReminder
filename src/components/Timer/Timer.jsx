afd103962aece878c8b0d8a994f7eb0ca1a65cec
creatine-cycle <>
2023-03-08 15:02:58 +0000

    adjusted local storage with current time and added math to keep track of time passed

--- src/components/Timer/Timer.jsx
+++ src/components/Timer/Timer.jsx
@@ -1,46 +1,82 @@
-import React, {useState, useEffect, useRef} from "react";
-import {TimerText} from "./TimerStyles.js";
+import React, { useState, useEffect, useRef, useCallback } from "react";
+import { TimerText } from "./TimerStyles.js";
 
 const Timer = () => {
-    const initialTimer = localStorage.getItem("timer") ?? 86400;
-    const timeoutId = useRef(null);
-    const [timer, setTimer] = useState(initialTimer);
+  const initialTimer = localStorage.getItem("timer") ?? 86400; //86400
+  const timeoutId = useRef(null);
+  const [timer, setTimer] = useState(initialTimer);
+  const [timeChecked, setTimeChecked] = useState(false);
 
-    const countTimer = React.useCallback(() => {
-        if (timer <= 0) {
-            localStorage.clear("timer");
-        } else {
-            setTimer(timer - 1);
-            localStorage.setItem("timer", timer);
-        }
-    }, [timer]);
+  const countTimer = useCallback(() => {
+    if (timer <= 0) {
+      localStorage.clear("timer");
+    } else if (!timeChecked) {
+      storeCurrentTime()
+    } else {
+      setTimer(timer - 1);
+      if (timer === 1) {
+        setTimer(86400)
+        new Notification("It's time to take your creatine!")
+      }
+      localStorage.setItem("timer", timer);
+    };
+  }, [timer]);
 
   const formatTime = (seconds) => {
     const minutes = Math.floor(seconds / 60);
     const remainingSeconds = seconds % 60;
     const formattedMinutes = formatMinutes(minutes);
-  
+
     return `${formattedMinutes}:${String(remainingSeconds).padStart(2, '0')}`;
   };
-  
+
   const formatMinutes = (minutes) => {
     const hours = Math.floor(minutes / 60);
     const remainingMinutes = minutes % 60;
-  
+
     const formattedHours = String(hours).padStart(2, '0');
     const formattedMinutes = String(remainingMinutes).padStart(2, '0');
-  
+
     return `${formattedHours}:${formattedMinutes}`;
   };
 
-    useEffect(() => {
-        timeoutId.current = window.setTimeout(countTimer, 1000);
-        // cleanup function
-        return () => window.clearTimeout(timeoutId.current);
-    }, [timer, countTimer]);
+  useEffect(() => {
+    timeoutId.current = window.setTimeout(countTimer, 1000);
+    // cleanup function
+    return () => window.clearTimeout(timeoutId.current);
+  }, [timer, countTimer]);
 
-    return (
-      <TimerText>Next Creatine Dose in {formatTime(timer)}</TimerText>);
+  const storeCurrentTime = () => {
+    if (!localStorage.getItem("currentTime")) {
+      const currentTime = new Date();
+      localStorage.setItem("currentTime", currentTime.toISOString())
+      setTimer(timer => { return timer - 1 })
+      setTimeChecked(true)
+    } else if (localStorage.getItem("currentTime") && !timeChecked) {
+      const currentTime = new Date().toISOString();
+      const currentSeconds = Date.parse(currentTime) / 1000;
+      const prevSeconds = Date.parse(localStorage.getItem("currentTime")) / 1000;
+      const timeDifference = Math.round(currentSeconds - prevSeconds);
+      localStorage.setItem("currentTime", currentTime)
+      const newTimer = timer - timeDifference
+      console.log(newTimer);
+      setTimer(prevTimer => {
+        const newTimer = prevTimer - timeDifference;
+        console.log(newTimer);
+        if (newTimer <= 0) {
+          new Notification("It's time to take your creatine!");
+          return 86400;
+        } else {
+          localStorage.setItem("timer", newTimer);
+          return newTimer;
+        }
+      });
+      setTimeChecked(true);
+    };
+  };
+
+  return (
+    <TimerText>Next Creatine Dose in {formatTime(timer)}</TimerText>);
 }
 
 export default Timer;
--- src/components/Window/Window.jsx
+++ src/components/Window/Window.jsx
@@ -1,9 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { WindowContainer, WindowDiv, WindowDetails, WindowName, WindowCross, WindowText, ButtonWrapper, WindowButton } from "./WindowStyles.js";
 import Timer from "../Timer/Timer"
 
 const Window = () => {
-  const timer = true
+  const [isRunning, setIsRunning] = useState(() => {
+    const savedIsRunning = localStorage.getItem("isRunning");
+    return savedIsRunning !== null ? JSON.parse(savedIsRunning) : false;
+  });
+
+  const [permission, setPermission] = useState(false)
+
+  useEffect(() => {
+    localStorage.setItem('isRunning', JSON.stringify(isRunning));
+  }, [isRunning]);
+
+  // useEffect(() => {
+  //   if (isRunning & Notification.permission === "granted" & !localStorage.getItem("timer")) {
+  //     new Notification("It's time to take your creatine!");
+  //   };
+  // })
+
+  const handleClick = () => {
+    setIsRunning(true);
+
+    if (!("Notification" in window)) {
+      alert("This browser does not support desktop notification");
+    } else if (Notification.permission === "granted") {
+      setPermission(true)
+      new Notification("Creatine taken!");
+    } else if (Notification.permission !== "denied") {
+      Notification.requestPermission().then(permission => {
+        if (permission === "granted") {
+          setPermission(true)
+          new Notification("Creatine taken!");
+        }
+      });
+    }
+  };
 
   return (
     <WindowContainer>
@@ -12,14 +45,14 @@ const Window = () => {
           <WindowName>System Message</WindowName>
           <WindowCross src="./assets/cross.svg" alt="cross" />
         </WindowDetails>
-        {timer ? (<>
-        <WindowText>Oh hell yeah, you're going to be jacked and I'm not going to let you forget!</WindowText>
-          <Timer />
-        </>): (
+        {isRunning ? (<>
+          <WindowText>Oh hell yeah, you're going to be jacked and I'm not going to let you forget!</WindowText>
+          <Timer isRunning={isRunning} />
+        </>) : (
           <>
             <WindowText>Take your damn creatine! When you have done so, click okay so you will never forget again.</WindowText>
             <ButtonWrapper>
-              <WindowButton onClick={handleStartClick()}>Ok</WindowButton>
+              <WindowButton onClick={handleClick}>Ok</WindowButton>
             </ButtonWrapper>
           </>
         )}
